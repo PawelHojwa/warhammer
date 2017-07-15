2795c0a0a3a48a539de11758a94b2df3281b408e
PawelHojwa <pablo-dx@o2.pl>
2017-07-16 00:08:09 +0200

    Zmiana miejsc odnośników na stronie głównej

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,8 +2,8 @@
 <h1><?php echo $title; ?> </h1>
 <?php
 echo br();
-	echo anchor('trade/price_list', $price_list, array('class' => 'btn_anchor btn btn-primary anchor'));
 	echo anchor('create_player/create', $create, array('class' => 'btn_anchor btn btn-primary anchor'));
+	echo anchor('trade/price_list', $price_list, array('class' => 'btn_anchor btn btn-primary anchor'));
 	echo anchor('speed/speed_tables', $speed, array('class' => 'btn_anchor btn btn-primary anchor'));
 ?>
 </div>
