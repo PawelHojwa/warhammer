cd31e5b59e38df44c8e3076e271f5c5dfc8ba3b9
PawelHojwa <pablo-dx@o2.pl>
2017-11-01 20:42:38 +0100

    Poprawiono wyciaganie danych z bazy w bibliotece Formable, elastyczny wyciąg danych bez względu na nr id danych

--- application/libraries/Formable.php
+++ application/libraries/Formable.php
@@ -5,10 +5,34 @@ class Formable {
 	public function datas() {
 		$CI = &get_instance();
 		$CI -> load -> model('universal_model');
-		$race = $CI -> universal_model -> arr_conv('rasa', 'raceName', 1);
-		$gender = $CI -> universal_model -> arr_conv('gender', 'genderName', 1);
-		$classes = $CI -> universal_model -> arr_conv('classes', 'className', 1);
-		$nature = $CI -> universal_model -> arr_conv('charakter', 'natureName', 1);
+		$races = $CI -> universal_model -> get_data('rasa');
+		$race_id = $race_name = array();
+		foreach ($races as $row) {
+			$race_id[] = $row['raceID'];
+			$race_name[] = $row['raceName'];
+		}
+		$race = array_combine($race_id, $race_name);
+		$genders = $CI -> universal_model -> get_data('gender');
+		$gender_id = $gender_name = array();
+		foreach ($genders as $row) {
+			$gender_id[] = $row['genderID'];
+			$gender_name[] = $row['genderName']; 
+		}
+		$gender = array_combine($gender_id, $gender_name);
+		$classess = $CI -> universal_model -> get_data('classes');
+		$class_id = $class_name = array();
+		foreach ($classess as $row) {
+			$class_id[] = $row['classID'];
+			$class_name[] = $row['className']; 
+		}
+		$classes = array_combine($class_id, $class_name);
+		$natures = $CI -> universal_model -> get_data('charakter');
+		$nature_id = $nature_name = array();
+		foreach ($natures as $row) {
+			$nature_id[] = $row['natureID'];
+			$nature_name[] = $row['natureName'];
+		}
+		$nature = array_combine($nature_id, $nature_name); 	
 		$data['title'] = 'Tworzenie postaci';
 		$data['user'] = $_SESSION['user'];
 		$data['id'] = $_SESSION['userID'];
