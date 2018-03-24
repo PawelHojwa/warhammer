b8caef79188cb1c1048a069a857acf104c12e329
PawelHojwa <pablo-dx@o2.pl>
2018-03-24 18:34:12 +0100

    Dodano wyswietlanie bledow zwiazanych z nieuzupelnieniem formularza w edycji umiejetnosci

--- application/views/edit/edit_skill.php
+++ application/views/edit/edit_skill.php
@@ -3,6 +3,7 @@
 echo heading($title, 3);
 echo anchor('admin_panel/add_class', '<- Powrót', array('title' => 'Powrót panelu administracyjnego'));
 echo br(2);
+echo validation_errors("<p class='alert alert-danger'>", "</p>");
 echo form_open('edit_panel/edit_skill_info');
 echo form_input('skill_name', $skillName, array('placeholder' => 'Nazwa umiejętności...'));
 echo br(2);
